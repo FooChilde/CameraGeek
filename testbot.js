c03cbb030e2493e9e83bec1b9debf842ef9802bf
Michael L. Foo <michael.l.foo@gmail.com>
2021-01-08 14:17:42 -0800

    TestBot update
    Experiment with command.js and reply-message.js modules
    further work on testbot.js

--- testbot.js
+++ testbot.js
@@ -8,13 +8,11 @@ const config = require("./testConfig.json");
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log('The client is ready!');
-})
+const prefix = ">";
 
-client.login(config.BOT_TOKEN);
+client.on(`ready`, () => {
+  console.log(`Client is ready!`)
 
-const prefix = ":";
 
 //Message functions
 client.on("message", function(message) {
@@ -22,18 +20,42 @@ client.on("message", function(message) {
 //Ignore other bots
   if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-//
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-//Help command
+//Help function
   if (command === "help") {
-    message.channel.send(`Here is a list of my supported commands:\n**args-info:**`);
-    return;
+    if (args === "diopter") {
+      //diopter help
+      return;
+    }
+    else if (args === "findfeet") {
+      //findfeet help
+      return;
+    }
+    else if (args === "findmeters") {
+      //findmeters help
+      return;
+    }
+    else {
+      const embed = new Discord.MessageEmbed()
+          .setTitle(`CameraGeek Command Help`)
+          .setColor('#DAF7A6')
+          .addFields(
+              {name: `Here is a list of my supported commands:`,
+              value: `-----------------------------------\n
+                      diopter <unit> <distance> - Finds close focus with different diopter strengths.\n
+                      findfeet <distance in meters> - Converts meters to feet.\n
+                      findmeters <distance in feet> - Converts feet to meters.\n
+                      miccheck - Checks to see if CameraGeek is on walkie!\n
+                      ping - Shows message latency.\n`}
+          )
+          message.channel.send(embed);
+    }
   }
 
 //Ping function
-  else if (command === "ping") {
+  if (command === "ping") {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
   }
@@ -41,35 +63,46 @@ client.on("message", function(message) {
 //Tests for arguments
   else if (command === 'args-info') {
     if (!args.length) {
-      message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-      return;
+      return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
     }
 
     message.channel.send(`Command name: ${command}\nArguments: ${args}`);
   }
 
-//Start CameraGeek commands
+//Start CameraGeek functions
 
 //Converts feet into meters
-  else if (command === "meters") {
+  else if (command === "findmeters") {
     if (!args.length) {
       return message.channel.send(`You didn't provide a distance in feet (decimal), ${message.author}!`);
     }
 
     let [distanceFeet] = args;
     let distanceMeters = Number((distanceFeet / 3.3).toFixed(1));
-    message.reply(`\n${args}ft is ~${distanceMeters}m.`);
+    const embed = new Discord.MessageEmbed()
+        .setColor('#DAF7A6')
+        .addFields(
+            {name: `Feet to Meters`,
+            value: `${distanceFeet}ft is equal to ${distanceMeters}m.`}
+        )
+        message.channel.send(embed);
   }
 
 //Converts meters into feet
-  else if (command === "feet") {
+  else if (command === "findfeet") {
     if (!args.length) {
       return message.channel.send(`You didn't provide a distance in meters (decimal), ${message.author}!`);
     }
 
     let [distanceMeters] = args;
     let distanceFeet = Number((distanceMeters * 3.3).toFixed(1));
-    message.reply(`\n${args}m is ~${distanceFeet}ft.`);
+    const embed = new Discord.MessageEmbed()
+        .setColor('#DAF7A6')
+        .addFields(
+            {name: `Meters to Feet`,
+            value: `${distanceMeters}m is equal to ${distanceFeet}ft.`}
+        )
+        message.channel.send(embed);
   }
 
 //Calculates close focus for diopters, works in inches, feet and meters
@@ -91,7 +124,18 @@ client.on("message", function(message) {
       let dioFullFt = Number((dioFull * 3.3).toFixed(1));
       let dioTwoFt = Number((dioTwo * 3.3).toFixed(1));
       let dioThreeFt = Number((dioThree * 3.3).toFixed(1));
-      message.reply(`minimum focus will be the following...\n+1/2 Dio: ${dioHalfFt}ft.\n+1 Dio: ${dioFullFt}ft.\n+2 Dio: ${dioTwoFt}ft.\n+3 Dio: ${dioThreeFt}ft.`);
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Close Focus with Diopter Strengths`)
+        .setColor('#DAF7A6')
+        .addFields(
+            {name: `Original Close Focus is ${closeFocus}ft.`,
+            value: `---------------------------\n
+                    +1/2 Diopter: ${dioHalfFt}ft.\n
+                    +1 Diopter: ${dioFullFt}ft.\n
+                    +2 Diopter: ${dioTwoFt}ft.\n
+                    +3 Diopter: ${dioThreeFt}ft.`}
+        )
+        message.channel.send(embed);
       return;
     }
 
@@ -101,11 +145,22 @@ client.on("message", function(message) {
       let dioFull = (closeFocusM/((1 * closeFocusM) + 1));
       let dioTwo = (closeFocusM/((2 * closeFocusM) + 1));
       let dioThree = (closeFocusM/((3 * closeFocusM) + 1));
-      let dioHalfFt = Number(((dioHalf * 3.3) * 12).toFixed(0));
-      let dioFullFt = Number(((dioFull * 3.3) * 12).toFixed(0));
-      let dioTwoFt = Number(((dioTwo * 3.3) * 12).toFixed(0));
-      let dioThreeFt = Number(((dioThree * 3.3) * 12).toFixed(0));
-      message.reply(`minimum focus will be the following...\n+1/2 Dio: ${dioHalfFt}in.\n+1 Dio: ${dioFullFt}in.\n+2 Dio: ${dioTwoFt}in.\n+3 Dio: ${dioThreeFt}in.`);
+      let dioHalfIn = Number(((dioHalf * 3.3) * 12).toFixed(0));
+      let dioFullIn = Number(((dioFull * 3.3) * 12).toFixed(0));
+      let dioTwoIn = Number(((dioTwo * 3.3) * 12).toFixed(0));
+      let dioThreeIn = Number(((dioThree * 3.3) * 12).toFixed(0));
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Close Focus with Diopter Strengths`)
+        .setColor('#DAF7A6')
+        .addFields(
+            {name: `Original Close Focus is ${closeFocus}in.`,
+            value: `---------------------------\n
+                    +1/2 Diopter: ${dioHalfIn}in.\n
+                    +1 Diopter: ${dioFullIn}in.\n
+                    +2 Diopter: ${dioTwoIn}in.\n
+                    +3 Diopter: ${dioThreeIn}in.`}
+        )
+        message.channel.send(embed);
       return;
     }
 
@@ -114,15 +169,29 @@ client.on("message", function(message) {
       let dioFull = Number((closeFocus/((1 * closeFocus) + 1)).toFixed(2));
       let dioTwo = Number((closeFocus/((2 * closeFocus) + 1)).toFixed(2));
       let dioThree = Number((closeFocus/((3 * closeFocus) + 1)).toFixed(2));
-      message.reply(`minimum focus will be the following...\n+1/2 Dio: ${dioHalf}m.\n+1 Dio: ${dioFull}m.\n+2 Dio: ${dioTwo}m.\n+3 Dio: ${dioThree}m.`);
+      const embed = new Discord.MessageEmbed()
+        .setTitle(`Close Focus with Diopter Strengths`)
+        .setColor('#DAF7A6')
+        .addFields(
+            {name: `Original Close Focus is ${closeFocus}m.`,
+            value: `---------------------------\n
+                    +1/2 Diopter: ${dioHalf}.\n
+                    +1 Diopter: ${dioFull}.\n
+                    +2 Diopter: ${dioTwo}.\n
+                    +3 Diopter: ${dioThree}.`}
+        )
+        message.channel.send(embed);
       return;
     }
 
   }
 
 //Mic Check
-  else if (command === "check") {
+  else if (command === "miccheck") {
     message.reply(`Good check!`);
     return;
   }
-});
+})
+
+})
+client.login(config.token);
